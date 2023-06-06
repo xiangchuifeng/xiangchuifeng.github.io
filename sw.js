30bfdacc8084a8ba5352b48ddfc1485425d9394e
xiangchuifeng <>
2023-06-06 11:44:45 +0800

    gx

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230606.113808';
+const cacheName = 'chirpy-20230606.114445';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
